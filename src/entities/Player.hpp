e54f679984f1e3f27656b76c8b079d5be7e2d35b
Grégory Pietquin <pietquin.gregory@outlook.be>
2025-10-05 21:57:18 +0200

    0.1.1

--- src/entities/Player.hpp
+++ src/entities/Player.hpp
@@ -2,12 +2,34 @@
 #include "raylib.h"
 #include "world/Types.hpp"
 #include <vector>
+#include <map>
+#include <string>
 
-struct Player {
-    Rectangle rect;
-    float speed;
+enum class Direction { DOWN, LEFT, RIGHT, UP };
+enum class Action { IDLE, RUN, ATTACK1, ATTACK2 };
+
+struct Animation {
+    Texture2D texture;
+    int frameWidth;
+    int frameHeight;
+    int frameCount;
+    int currentFrame;
+    float frameTime;
+    float timer;
 };
 
+struct Player {
+    Vector2 position;
+    float speed;
+    Direction direction;
+    Action action;
+    std::map<std::string, Animation> animations;
+    Rectangle rect; // Rectangle visuel
+    Rectangle hitboxCollision; // Rectangle pour les collisions
+};
+
+// Fonctions
 void InitPlayer(Player& player, float startX, float startY);
 void UpdatePlayer(Player& player, const std::vector<CollisionBox>& collisions);
 void DrawPlayer(const Player& player);
+void UnloadPlayer(Player& player);
